901ea070b2cf580906efa111abb7aac411fb1e00
incubator4 <aries0robin@gmail.com>
2023-06-13 18:41:52 +0800

    graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ mod server;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::Server;
 
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let addr = ([0, 0, 0, 0], 3000).into();
 
     let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(server::echo)) });
@@ -16,7 +23,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Listening on http://{}", addr);
 
-    server.await?;
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
 
-    Ok(())
+    // Run this server for... forever!
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
 }
